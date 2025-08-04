f2a95f9756515ba5f4f7013aacd4f954a0318b60
ToufeeqParker786 <600947@student.belgiumcampus.ac.za>
2025-08-04 11:36:19 +0200

    added navbar to homepage

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className='flex border-t justify-between font-medium p-6'>
+      <div>
+        <p> funroad </p>
+      </div>
+    </footer>
+  )
+}
+
