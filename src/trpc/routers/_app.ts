1581a3cbd1fc9a729df0e4faec91ee3db5db35d0
ToufeeqParker2004 <toufeeqparker041104@gmail.com>
2025-09-06 10:51:05 +0200

    Checkout page

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,12 +5,14 @@ import { categoriesRouter } from '@/modules/categories/server/procedure';
 import { productsRouter } from '@/modules/products/server/procedure';
 import { tagsRouter } from '@/modules/tags/server/procedure';
 import { tenantsRouter } from '@/modules/tenants/server/procedure';
+import { checkoutRouter } from '@/modules/checkout/server/procedure';
 
 
 export const appRouter = createTRPCRouter({
   auth:authRouter,
   tags :tagsRouter,
   tenants:tenantsRouter,
+  checkout: checkoutRouter,
   products: productsRouter,
   categories : categoriesRouter
 });
